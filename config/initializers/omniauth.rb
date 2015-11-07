2f38df6c33e56b4d70b44613cf9c6a563788e785
ANDELA UBUNTU <ojewaleolaide@gmail.com>
2015-11-07 13:53:13 +0100

    Add github omniauth.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  #Use appropriate controller for destroy action.
-  # OmniAuth.config.on_failure = SessionsController.action(:destroy)
+  OmniAuth.config.on_failure = SessionsController.action(:destroy)
   provider :google_oauth2, ENV["google_client_id"], ENV["google_secret"],
     {
       :scope => "email, profile, plus.me, http://gdata.youtube.com",
@@ -8,4 +7,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
       :image_aspect_ratio => "square",
       :image_size => 50
     }
+
+  provider :heroku, ENV['HEROKU_OAUTH_ID'], ENV['HEROKU_OAUTH_SECRET']
+
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "user,repo,gist"
 end
