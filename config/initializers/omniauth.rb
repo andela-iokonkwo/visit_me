63e3206f43a86a8f7ac6d53fac82c8f2edfb9a70
ANDELA UBUNTU <ojewaleolaide@gmail.com>
2015-11-07 14:35:13 +0100

    Remove token columm from users table

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,10 +2,11 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   OmniAuth.config.on_failure = SessionsController.action(:destroy)
   provider :google_oauth2, ENV["google_client_id"], ENV["google_secret"],
     {
-      :scope => "email, profile, plus.me, http://gdata.youtube.com",
-      :prompt => "select_account",
-      :image_aspect_ratio => "square",
-      :image_size => 50
+      scope: "calendar",
+      prompt: "select_account",
+      image_aspect_ratio: "square",
+      image_size: 50,
+      offline_access: true
     }
 
   provider :heroku, ENV['HEROKU_OAUTH_ID'], ENV['HEROKU_OAUTH_SECRET']
