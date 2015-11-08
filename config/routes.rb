2f3104b4de63bdb61bd415127a6877f678de6a1d
Chinedu <chinedudaniel7@gmail.com>
2015-11-08 14:55:26 +0100

    Move stylesheet files to apprpriate folders

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,18 @@
 Rails.application.routes.draw do
   root "pages#index"
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  get 'receptionists/show'
+  get "receptionists/show"
 
-  get 'receptionists/index'
+  get "receptionists/index"
 
-  get 'receptionists/create'
+  get "receptionists/create"
 
-  get 'receptionists/delete'
+  get "receptionists/delete"
 
-  get 'receptionists/update'
+  get "receptionists/update"
 
-  get 'receptionists/edit'
+  get "receptionists/edit"
 
-  get 'receptionist/index'
-
-  get 'receptionist/create'
-
-  get 'receptionist/update'
-
-  get 'receptionist/destroy'
-
-  get 'receptionist/show'
 
   resources :meetings
 
@@ -32,5 +21,4 @@ Rails.application.routes.draw do
   get "/auth/:provider/callback", to: "sessions#create"
 
   get "logout" => "sessions#destroy"
-
 end
